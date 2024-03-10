f4fe8109e6086a32f69acdedd2600af3020b8c82
trickyrat <wangjian0513@outlook.com>
2024-03-10 23:54:11 +0800

    apply seeding data services while starting at first time

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,11 +1,19 @@
 import { Injectable, OnApplicationBootstrap } from '@nestjs/common';
 import { UsersService } from './users/users.service';
+import { AuthorsService } from './authors/authors.service';
+import { BooksService } from './books/books.service';
 
 @Injectable()
 export class AppService implements OnApplicationBootstrap {
-  constructor(private usersService: UsersService) {}
+  constructor(private usersService: UsersService,
+    private authorService: AuthorsService,
+    private bookService: BooksService) {}
 
   onApplicationBootstrap() {
     this.usersService.seedData();
+
+    this.authorService.seedData();
+
+    this.bookService.seedData();
   }
 }
