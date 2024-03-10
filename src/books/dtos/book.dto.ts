a9d40695aa52078ec1f377ec616059290a86313e
trickyrat <wangjian0513@outlook.com>
2024-03-11 01:06:44 +0800

    update path dto to dtos

--- src/books/dtos/book.dto.ts
+++ src/books/dtos/book.dto.ts
@@ -4,11 +4,15 @@ export class BookDto {
   @ApiProperty()
   id: number;
   @ApiProperty()
+  isbn: string;
+  @ApiProperty()
   title: string;
+  @ApiProperty()
+  price: number;
+  @ApiProperty()
+  description: string;
   @ApiProperty()
   publishDate: string;
   @ApiProperty()
   isActive: boolean;
-  @ApiProperty()
-  author: string;
 }
